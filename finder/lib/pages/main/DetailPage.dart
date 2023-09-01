fe299f4d293c5adbf31a9c9cf279085762fc7c09
JangHoon Park <gretea4489@gmail.com>
2023-09-01 23:37:36 +0900

    [0901] feat: 상세 정보 페이지 병상 추이 데이터 보여주기

--- finder/lib/pages/main/DetailPage.dart
+++ finder/lib/pages/main/DetailPage.dart
@@ -540,7 +540,7 @@ class _DetailPageState extends State<DetailPage> {
                                                 mainAxisAlignment: MainAxisAlignment.center,
                                                 children: [
                                                   Text(
-                                                    "3시간 30분",
+                                                    "${getHospitalDetailSnapshot.data!.bedData.successTime}",
                                                     style: TextStyle(
                                                       fontSize: 16,
                                                       fontWeight: FontWeight.w600,
@@ -566,8 +566,8 @@ class _DetailPageState extends State<DetailPage> {
                                             aspectRatio: 16 / 9,
                                             child: DChartPie(
                                                   data: [
-                                                    {'domain': 'OnTime', 'measure': 85},
-                                                    {'domain': 'OffTime', 'measure': 15},
+                                                    {'domain': 'OnTime', 'measure': getHospitalDetailSnapshot.data!.bedData.otherPercent},
+                                                    {'domain': 'OffTime', 'measure': getHospitalDetailSnapshot.data!.bedData.percent},
                                                   ],
                                                   fillColor: (pieData, index) {
                                                     switch(pieData['domain']) {
@@ -603,7 +603,7 @@ class _DetailPageState extends State<DetailPage> {
                                           margin: EdgeInsets.only(top: 20),
                                           child: Row(
                                             children: [
-                                              RotatedBox(quarterTurns: 3, child: Text('Quantity')),
+                                              RotatedBox(quarterTurns: 3, child: Text('병상 수')),
                                               SizedBox(width: 8),
                                               Expanded(
                                                 child: Column(
@@ -657,28 +657,32 @@ class _DetailPageState extends State<DetailPage> {
                                                           {
                                                             'id': 'Line 1',
                                                             'data': [
-                                                              {'domain': 0, 'measure': 14},
-                                                              {'domain': 20, 'measure': 11},
-                                                              {'domain': 40, 'measure': 8},
-                                                              {'domain': 60, 'measure': 5},
-                                                              {'domain': 80, 'measure': 18},
+                                                              {'domain': -120, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[0]},
+                                                              {'domain': -105, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[1]},
+                                                              {'domain': -90, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[2]},
+                                                              {'domain': -75, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[3]},
+                                                              {'domain': -60, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[4]},
+                                                              {'domain': -45, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[5]},
+                                                              {'domain': -30, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[6]},
+                                                              {'domain': -15, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[7]},
+                                                              {'domain': 0, 'measure': getHospitalDetailSnapshot.data!.bedData.twoAgoList[8]},
                                                             ],
                                                           },
-                                                          {
-                                                            'id': 'Line 2',
-                                                            'data': [
-                                                              {'domain': 0, 'measure': 2},
-                                                              {'domain': 20, 'measure': 5},
-                                                              {'domain': 40, 'measure': 1},
-                                                              {'domain': 60, 'measure': 1},
-                                                              {'domain': 80, 'measure': 5},
-                                                            ],
-                                                          },
+                                                          // {
+                                                          //   'id': 'Line 2',
+                                                          //   'data': [
+                                                          //     {'domain': 0, 'measure': 2},
+                                                          //     {'domain': 20, 'measure': 5},
+                                                          //     {'domain': 40, 'measure': 1},
+                                                          //     {'domain': 60, 'measure': 1},
+                                                          //     {'domain': 80, 'measure': 5},
+                                                          //   ],
+                                                          // },
                                                         ],
                                                         includePoints: true,
                                                       ),
                                                     ),
-                                                    Text('Day'),
+                                                    Text('분 전'),
                                                   ],
                                                 ),
                                               ),
